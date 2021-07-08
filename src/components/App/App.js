165594c8dc466b28ddd3c0ceb774f1c94fa8aee3
Steve Savstrom <steve.savstrom@gmail.com>
2021-07-08 13:18:07 -0500

    header complete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,68 +1,64 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Header from '../Header/Header';
+import "./App.css";
 
 function App() {
   let [guestList, setGuestList] = useState([]);
-  let [newGuestName, setNewGuestName] = useState('');
-  let [newGuestMeal, setNewGuestMeal] = useState('false');
+  let [newGuestName, setNewGuestName] = useState("");
+  let [newGuestMeal, setNewGuestMeal] = useState("false");
 
   //On load, get guests
   useEffect(() => {
-    getGuests()
-  }, [])
+    getGuests();
+  }, []);
 
   const getGuests = () => {
-    axios.get('/guests')
-      .then(response => {
-        setGuestList(response.data)
+    axios
+      .get("/guests")
+      .then((response) => {
+        setGuestList(response.data);
       })
-      .catch(err => {
-        alert('error getting guests');
+      .catch((err) => {
+        alert("error getting guests");
         console.log(err);
-      })
-  }
-
+      });
+  };
 
   const addGuest = () => {
-    axios.post('/guests', { name: newGuestName, kidsMeal: newGuestMeal })
-      .then(response => {
+    axios
+      .post("/guests", { name: newGuestName, kidsMeal: newGuestMeal })
+      .then((response) => {
         // clear inputs
-        setNewGuestName('');
+        setNewGuestName("");
         setNewGuestMeal(false);
 
         getGuests();
       })
-      .catch(err => {
-        alert('Error Adding Guest');
+      .catch((err) => {
+        alert("Error Adding Guest");
         console.log(err);
-      })
+      });
   };
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (newGuestName) {
       addGuest();
+    } else {
+      alert("The new guest needs a name!");
     }
-    else {
-      alert('The new guest needs a name!');
-    }
-  }
+  };
 
-  console.log(newGuestMeal)
+  console.log(newGuestMeal);
   return (
     <div className="App">
-      <header>
-        <h1>Prim Proper Props</h1>
-      </header>
+      <Header />
       <h2>Party Leader</h2>
       {guestList[0] && <h3>{guestList[0].name}</h3>}
       <h2>Add a new guest</h2>
       <form onSubmit={handleSubmit}>
-        <label>
-          Name
-        </label>
+        <label>Name</label>
         <input
           type="text"
           placeholder="Name"
@@ -71,13 +67,13 @@ function App() {
         />
         <div>
           Would this guest like a kid's meal?
-          <div >
+          <div>
             <div>
               <label>
                 <input
                   type="radio"
                   value={true}
-                  checked={newGuestMeal === 'true'}
+                  checked={newGuestMeal === "true"}
                   name="kidsMeal"
                   onChange={(evt) => setNewGuestMeal(evt.target.value)}
                 />
@@ -89,7 +85,7 @@ function App() {
                 <input
                   type="radio"
                   value={false}
-                  checked={newGuestMeal === 'false'}
+                  checked={newGuestMeal === "false"}
                   name="kidsMeal"
                   onChange={(evt) => setNewGuestMeal(evt.target.value)}
                 />
@@ -109,7 +105,7 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {guestList.map(guest => (
+          {guestList.map((guest) => (
             <tr key={guest.id}>
               <td>{guest.name}</td>
               <td>{String(guest.kidsMeal)}</td>
@@ -118,15 +114,9 @@ function App() {
         </tbody>
       </table>
       <h2>Dinner Supplies</h2>
-      <div>
-        Spoons: {guestList.length * 2}
-      </div>
-      <div>
-        Forks: {guestList.length * 2}
-      </div>
-      <div>
-        Knives: {guestList.length * 2}
-      </div>
+      <div>Spoons: {guestList.length * 2}</div>
+      <div>Forks: {guestList.length * 2}</div>
+      <div>Knives: {guestList.length * 2}</div>
       <footer>
         <h3>Have fun!</h3>
         <p>Don't forget to mind your Ps and Qs!</p>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+
+function Header(props) {
+  return (
+    <header>
+      <h1>Prim Proper Props</h1>
+    </header>
+  );
+}
+
+export default Header;
